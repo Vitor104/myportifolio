a1e1d8914ad64299fd33f915bfa5a5c82b90afbe
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-01-20 13:20:39 -0300

    comecei a main page

---
+++ main-page/main.js
@@ -0,0 +1,8 @@
+function myNav () {
+    let nav = document.getElementById('menu-nav');
+    if (nav.style.display === 'none') {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+}
