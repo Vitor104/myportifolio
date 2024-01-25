e34270c95bfc4a0ad0b52c327e28350f34825ede
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-01-25 19:10:25 -0300

    adicionei funcionalidades de js

--- enter-page/index.js
+++ enter-page/index.js
@@ -1,13 +1,12 @@
-let figuraWord = document.getElementById('word-figura');
-let figura = document.getElementById('figura');
+
 
 function myFunction() {
-    if (figuraWord.style.display === "none") {
-      figuraWord.style.display = "block";
-    } else {
-      figuraWord.style.display = "none";
-    }
+    let figuraWord = document.getElementById('word-figura');
+    figuraWord.style.display = "block";
 }
 
+function myOutFunction() {
+    let figuraWord = document.getElementById('word-figura');
+    figuraWord.style.display = 'none';
+}
 
-figura.addEventListener('mouseover', myFunction());
